ec38d9a8332967a24e4dcefc2560e5ac75ebb093
Ei-Thandar12 <eitk12121999@gmail.com>
2026-05-29 14:27:31 +0900

    Add files via upload

---
+++ 01_omikuji01/js/omikuji.js
@@ -0,0 +1,40 @@
+"use strict";
+
+window.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    this.setTimeout(function () {
+      let popMessage = "いらっしゃい!　おみくじ引いてって！";
+      this.window.alert(popMessage);
+      this.window.alert(popMessage);
+    }, "3000");
+  },
+  false
+);
+
+const btn1 = document.getElementById("btn1");
+const omikujiText = document.getElementById("omikujiText");
+btn1.addEventListener(
+  "click",
+  function () {
+    let n = Math.floor(Math.random() * 3);
+    switch (n) {
+      case 0:
+        omikujiText.textContent = "Very Happy!!!";
+        omikujiText.style.color = "red";
+        omikujiText.style.fontSize = "80px";
+        break;
+      case 1:
+        omikujiText.textContent = "Happy!!!";
+        omikujiText.style.color = "yellow";
+        omikujiText.style.fontSize = "40px";
+        break;
+      case 2:
+        omikujiText.textContent = "UnHappy!!!";
+        omikujiText.style.color = "black";
+        omikujiText.style.fontSize = "15px";
+        break;
+    }
+  },
+  false
+);
